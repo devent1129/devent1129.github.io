073675509602cffb7b9250689a8d9b84d9ec7eab
devent1129 <43049316+devent1129@users.noreply.github.com>
2018-12-10 15:27:26 -0500

    Add files via upload

---
+++ Collection Project/jave.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $(".naone").hover(function() {
+    $(".infohover1").toggleClass("appear");
+  });
+    $(".saone").hover(function() {
+    $(".infohover2").toggleClass("appear");
+  });
+     $(".euone").hover(function() {
+    $(".infohover3").toggleClass("appear");
+  });
+      $(".asone").hover(function() {
+    $(".infohover4").toggleClass("appear");
+  });
+        $(".afrone").hover(function() {
+    $(".infohover5").toggleClass("appear");
+  });
+    $(".ausone").hover(function() {
+    $(".infohover6").toggleClass("appear");
+  });
+    $(".natwo").hover(function() {
+    $(".infohover7").toggleClass("appear");
+  });
+     $(".satwo").hover(function() {
+    $(".infohover8").toggleClass("appear");
+  });
+      $(".eutwo").hover(function() {
+    $(".infohover9").toggleClass("appear");
+  });
+
+
+}); 
