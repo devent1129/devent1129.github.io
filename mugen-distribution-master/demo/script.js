8531bdc60006c6abc0ffa7bd11c4ece814f2ef03
devent1129 <43049316+devent1129@users.noreply.github.com>
2019-04-01 14:52:11 -0400

    Add files via upload

--- mugen-distribution-master/demo/script.js
+++ mugen-distribution-master/demo/script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({attackRadius: 500, monsterHitRadius: 100, attackDuration:500, jumpHeight: 300});
+  mugen.initialize({attackRadius: 100, monsterHitRadius: 100, attackDuration:500, jumpHeight: 270, pointsPerMonsterKilled: 1, autoPlay: false, makeResponsive: true});
 });
