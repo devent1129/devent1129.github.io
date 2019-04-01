356748f18297b1a1719078824193396208ab72d0
devent1129 <43049316+devent1129@users.noreply.github.com>
2019-04-01 17:47:13 -0400

    Add files via upload

---
+++ balloon/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var mugen = new MUGEN();
+  mugen.initialize({titleScreenImageURL: "splashwide.png", attackRadius: 100, monsterHitRadius: 100, attackDuration:500, jumpHeight: 270, pointsPerMonsterKilled: 1, autoPlay: false, makeResponsive: true});
+});
